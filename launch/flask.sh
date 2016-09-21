3a0494ac7da8ad3740b750d4209dd20a37d6a268
Ajish <ajish@rootedinsights.com>
2016-09-21 11:49:09 -0400

    tweaks

--- launch/flask.sh
+++ launch/flask.sh
@@ -1,1 +1,1 @@
-docker run -d  --link mongo_instance_001 --name pymicroservice-inst1 pymicroservice 
+docker run -d  --link mongo_instance_001 -p 9999:8081 --name pymicroservice-inst1 pymicroservice 
--- launch/mongo.sh
+++ launch/mongo.sh
@@ -1,1 +1,1 @@
-docker run  -v /opt/srv/docker/mongodb:/var/lib/mongodb --name mongo_instance_001 -d --restart=always mongodb  
+docker run  -v /opt/srv/docker/mongodb:/var/lib/mongodb -p 27107:27107 --name mongo_instance_001 -d --restart=always mongodb  
